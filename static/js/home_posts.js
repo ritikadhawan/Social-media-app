7471a9bdb3daad408a1d844e1e08f04ee76c74c1
ritikadhawan <ritikadhawan28@gmail.com>
2020-02-22 00:08:05 +0530

    display posts using ajax

--- static/js/home_posts.js
+++ static/js/home_posts.js
@@ -12,6 +12,8 @@
                 data: newPostForm.serialize(),
                 success: (data)=>{
                     console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-lists-container>ul').prepend(newPost);
                 },
                 error: (error)=>{
                     console.log(error.responseText);
@@ -21,5 +23,36 @@
         });
     }
 
+
+    //function to display the post
+    //here the person who is making the post is already signed in thus we can remove the checks
+    let newPostDom = (post)=>{
+        return $(`<li id="post-${post._id}">
+            <p>
+               
+                <a href="/posts/destroy/${post.id}" class="delete-post-button">Delete Post</a>
+               
+                ${post.content}
+                <br>
+                <small>
+                    ${post.user.name}
+                </small>
+            </p>
+            <div class="post-comments">
+                    <form action="/comments/create" method="POST" >
+                    
+                        <input type="text" id="comment-content" name="content" placeholder="type here to add comment.." required>
+                        <input type="hidden" name="post" value="${post._id}">
+                        <input type="submit" value="Comment">
+                    </form>
+             
+            </div>
+            <div class="post-comments-list">
+            <ul id="post-comments-${post._id}">
+                
+            </ul>
+            </div>
+        </li>`);
+    }
     createPosts();
 }
